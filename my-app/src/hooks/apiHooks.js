6a2cf2486dcf912647d7ac1101705cad633c4ad0
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2026-04-14 12:56:01 +0300

    Add custom-hooks

---
+++ my-app/src/hooks/apiHooks.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react';
+import {fetchData} from '../utils/fetchData';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+    const getMedia = async () => {
+      try {
+        const mediaUrl = import.meta.env.VITE_MEDIA_API + '/media';
+        const media = await fetchData(mediaUrl);
+
+        console.log('media from API:', media);
+
+        const authBase = import.meta.env.VITE_AUTH_API + '/users/';
+
+        const mediaWithUsers = await Promise.all(
+          media.map(async (item) => {
+            const user = await fetchData(authBase + item.user_id);
+            return {
+              ...item,
+              username: user.username,
+            };
+          }),
+        );
+
+        console.log('media with users:', mediaWithUsers);
+
+        setMediaArray(mediaWithUsers);
+      } catch (error) {
+        console.error('Error loading media:', error);
+      }
+    };
+
+    getMedia();
+  }, []);
+  console.log(mediaArray);
+
+  return {mediaArray};
+};
+
+export {useMedia};
